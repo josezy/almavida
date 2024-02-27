68243d38ebdaf6f05b3f993e3734d3e66f33d5b1
Jose B <jose.zdy@gmail.com>
2024-02-27 14:01:57 -0500

    change units

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,7 @@
 
 const Menu = () => {
     const pdfURL = '/almavida-menu-v2.pdf'
-    const iframeStyle = { width: '100vw', height: '100vh' }
+    const iframeStyle = { width: '100svw', height: '100svh' }
     return <iframe src={pdfURL} style={iframeStyle} />
 }
 
