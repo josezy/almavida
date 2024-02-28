ab466812ece3b00e000aabc6115bae6769d56cbe
Jose B <jose.zdy@gmail.com>
2024-02-27 23:00:49 -0500

    migrate to tailwind

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,13 +9,18 @@ const Home = () => {
         window.location.href = '/menu'
     }
     return (
-        <div className='splash-container'>
-            <img src={bottomRight} alt='bottom right' className='bottom-right' />
-            <img src={bottomRight} alt='top left' className='top-left' />
-            <img src={topRight} alt='top right' className='top-right' />
-            <img src={bottomLeft} alt='bottom left' className='bottom-left' />
-            <img src={logo} alt='logo almavida' className='logo' />
-            <div className='menu' onClick={onClickMenu}>Menú</div>
+        <div className='flex flex-col items-center justify-center w-svw h-svh'>
+            <img src={bottomRight} alt='bottom right' className='absolute bottom-0 right-0 w-[50vw] max-w-[300px]' />
+            <img src={bottomRight} alt='top left' className='absolute top-0 left-0 w-[50vw] max-w-[300px] rotate-180' />
+            <img src={topRight} alt='top right' className='absolute top-0 right-0 w-[50vw]' />
+            <img src={bottomLeft} alt='bottom left' className='absolute left-0 bottom-0 w-[50vw]' />
+            <img src={logo} alt='logo almavida' className='w-4/5 max-w-[800px] landscape:max-h-[50svh] landscape:w-auto' />
+            <div
+                className='text-5xl md:text-7xl text-darkBlue font-bold hover:underline hover:opacity-80 cursor-pointer z-10'
+                onClick={onClickMenu}
+            >
+                Menú
+            </div>
         </div>
     )
 }
