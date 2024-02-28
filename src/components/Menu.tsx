b75f00f8122144949220b65b112edf3a72cd92f1
Jose B <jose.zdy@gmail.com>
2024-02-27 23:12:53 -0500

    use webp instead pdf >_<

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,11 @@
-import PDFObject from 'pdfobject'
 
 const Menu = () => {
-    const pdfURL = '/almavida-menu-v2.pdf'
-    if(!PDFObject.supportsPDFs){
-        window.location.href = pdfURL
-    }
-    const options = {
-        fallbackLink: "<p>Tu navegador no soporta PDFs. Por favor descarga el archivo: <a href='[url]'>Descargar menú</a></p>",
-    }
-    PDFObject.embed(pdfURL, document.body, options)
-    return null;
+    return (
+        <div className='w-full max-w-[1024px] m-auto '>
+            <img src="Artboard 2@3x.webp" alt="menu-02" />
+            <img src="Artboard 3@3x.webp" alt="menu-03" />
+        </div>
+    )
 }
 
 export default Menu
