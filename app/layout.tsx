713fce452dab104580bc6498e956d439332d299d
Jose B <jose.zdy@gmail.com>
2024-07-04 13:23:44 -0500

    revive icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,28 @@ export const metadata: Metadata = {
       },
     ],
   },
+  icons: {
+    icon: [
+      { rel: "apple-touch-icon", sizes: "57x57", url: "/apple-icon-57x57.png" },
+      { rel: "apple-touch-icon", sizes: "60x60", url: "/apple-icon-60x60.png" },
+      { rel: "apple-touch-icon", sizes: "72x72", url: "/apple-icon-72x72.png" },
+      { rel: "apple-touch-icon", sizes: "76x76", url: "/apple-icon-76x76.png" },
+      { rel: "apple-touch-icon", sizes: "114x114", url: "/apple-icon-114x114.png" },
+      { rel: "apple-touch-icon", sizes: "120x120", url: "/apple-icon-120x120.png" },
+      { rel: "apple-touch-icon", sizes: "144x144", url: "/apple-icon-144x144.png" },
+      { rel: "apple-touch-icon", sizes: "152x152", url: "/apple-icon-152x152.png" },
+      { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-icon-180x180.png" },
+      { rel: "icon", type: "image/png", sizes: "36x36", url: "/android-icon-36x36.png" },
+      { rel: "icon", type: "image/png", sizes: "48x48", url: "/android-icon-48x48.png" },
+      { rel: "icon", type: "image/png", sizes: "72x72", url: "/android-icon-72x72.png" },
+      { rel: "icon", type: "image/png", sizes: "96x96", url: "/android-icon-96x96.png" },
+      { rel: "icon", type: "image/png", sizes: "144x144", url: "/android-icon-144x144.png" },
+      { rel: "icon", type: "image/png", sizes: "192x192", url: "/android-icon-192x192.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", url: "/favicon-16x16.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "96x96", url: "/favicon-96x96.png" },
+    ],
+  },
 };
 
 export default function RootLayout({
