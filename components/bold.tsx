5a01235faf8ce1bf907917dbb0d9e697fc0e5597
Jose B <jose.zdy@gmail.com>
2024-08-10 10:02:14 -0500

    move button to its own component

---
+++ components/bold.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+type BoldCheckoutProps = {
+  orderId: string
+  currency: string
+  amount: string
+  apiKey: string
+  integritySignature: string
+  description: string
+  tax: string
+}
+
+const initBoldCheckout = () => {
+  if (document.querySelector('script[src="https://checkout.bold.co/library/boldPaymentButton.js"]')) {
+    console.warn('Bold Checkout script is already loaded.')
+    return
+  }
+
+  let js
+  js = document.createElement('script')
+  js.onload = () => {
+    window.dispatchEvent(new Event('boldCheckoutLoaded'))
+  }
+  js.onerror = () => {
+    window.dispatchEvent(new Event('boldCheckoutLoadFailed'))
+  }
+  js.src = 'https://checkout.bold.co/library/boldPaymentButton.js'
+  document.head.appendChild(js)
+}
+
+const BoldButton = () => {
+  const [boldCheckoutProps, setBoldCheckoutProps] = React.useState<BoldCheckoutProps | null>(null)
+  React.useEffect(() => {
+    window.addEventListener('boldCheckoutLoaded', async () => {
+      const boldCheckoutProps = await fetch('/api/get-bold-checkout').then(res => res.json())
+      setBoldCheckoutProps(boldCheckoutProps)
+    })
+    initBoldCheckout()
+  }, [])
+
+  const pay = () => {
+    const checkout = new (window as any).BoldCheckout({
+      redirectionUrl: window.location.href,
+      ...boldCheckoutProps,
+    })
+    checkout.open()
+  }
+
+  return (
+    <button onClick={pay} className="rounded-xl bg-orange-300 px-4 py-1 border border-orange-500">
+      Pagar
+    </button>
+  )
+}
+
+export default BoldButton
